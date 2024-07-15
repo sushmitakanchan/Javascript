c484addf05d1e661ee95c2192ff08b4c0e816206
sushmitakanchan <31158191+sushmitakanchan@users.noreply.github.com>
2024-07-15 10:04:25 +0000

    variables and datatypes done!

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountNo = 4759;
+let accountEmail = "sush@gmail.com";
+var accountPass = 1234;
+accountCity = "Mumbai";
+let accountState;
+
+//not to use var in javascript due to blockscope and functional issue
+//accountNo = 1245;
+console.log(accountNo);
+console.table([accountNo, accountEmail, accountPass, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+let name = "Sush"
+let age = 24
+let isLoggedIn = true
+
+console.log(typeof null);//object
+console.log(typeof undefined); //undefined
+//number = 2 to the power 53
+//string=""
+//bigint
+//boolean
+//null
+//undefined
+//symbol => unique
+//null=> standalone value
+//undefined
+
+//object
