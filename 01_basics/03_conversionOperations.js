ea6d4509d59045c76fce613d81d9ec9615449cac
sushmitakanchan <31158191+sushmitakanchan@users.noreply.github.com>
2024-07-15 10:52:07 +0000

    conversion operations done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = true
+let valueInNumber = Number(score)
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+
+//"33" - 33
+//"sush" - NaN
+//null - 0
+//undefined - NaN
+//true-1; false-0
+
+let isLoggedIn = ""
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn)
+console.log(typeof booleanLoggedIn)
+
+
+//1-true; 0-false;
+//""-false;
+//"sush" - true
+
+let somenumber = undefined
+let valueInString = String(somenumber)
+//console.log(valueInString)
+//console.log(typeof valueInString)
